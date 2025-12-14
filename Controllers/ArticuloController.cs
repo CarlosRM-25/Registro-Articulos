c9c28733ab0e49128823e8c585284843a273dd3e
CarlosRM-25 <158112977+CarlosRM-25@users.noreply.github.com>
2025-12-14 15:17:54 -0600

    Add files via upload

---
+++ Controllers/ArticuloController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using GamingZoneApp.Data;
+using GamingZoneApp.Models;
+
+namespace GamingZoneApp.Controllers
+{
+    public class ArticuloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(ArticuloRepository.ObtenerArticulos());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Articulo articulo)
+        {
+            if (ArticuloRepository.CodigoExiste(articulo.CodigoInterno))
+            {
+                ModelState.AddModelError("CodigoInterno", "El código interno ya existe");
+            }
+
+            if (articulo.AnioLanzamiento > DateTime.Now.Year)
+            {
+                ModelState.AddModelError("AnioLanzamiento", "El año no puede ser futuro");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(articulo);
+            }
+
+            ArticuloRepository.AgregarArticulo(articulo);
+            TempData["Mensaje"] = "Artículo registrado correctamente";
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
