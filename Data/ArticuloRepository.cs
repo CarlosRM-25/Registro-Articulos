1f06b59d37ad5468f90d2006a6197b969b97e0f0
CarlosRM-25 <158112977+CarlosRM-25@users.noreply.github.com>
2025-12-14 15:18:06 -0600

    Add files via upload

---
+++ Data/ArticuloRepository.cs
@@ -0,0 +1,24 @@
+﻿using GamingZoneApp.Models;
+
+namespace GamingZoneApp.Data
+{
+    public static class ArticuloRepository
+    {
+        private static List<Articulo> _articulos = new();
+
+        public static List<Articulo> ObtenerArticulos()
+        {
+            return _articulos;
+        }
+
+        public static bool CodigoExiste(string codigo)
+        {
+            return _articulos.Any(a => a.CodigoInterno == codigo);
+        }
+
+        public static void AgregarArticulo(Articulo articulo)
+        {
+            _articulos.Add(articulo);
+        }
+    }
+}
